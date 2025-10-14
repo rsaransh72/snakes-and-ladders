b4dcb8f76eea93de2104735f65f08d12c2ac2910
rsaransh <rsaransh.72@gmail.com>
2025-10-15 00:55:08 +0530

    Initial commit

---
+++ src/main/java/com/saransh/snakesandladders/snakes_and_ladders/App.java
@@ -0,0 +1,10 @@
+package com.saransh.snakesandladders.snakes_and_ladders;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/saransh/snakesandladders/snakes_and_ladders/AppTest.java
@@ -0,0 +1,19 @@
+package com.saransh.snakesandladders.snakes_and_ladders;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
