c3f6882dd064e58bc1c4e7781ed64506a82fe2cb
rsaransh <rsaransh.72@gmail.com>
2025-10-15 11:28:20 +0530

    feat: enforce rule - stay in place when move exceeds 100

--- src/main/java/com/saransh/snakesandladders/snakes_and_ladders/GameEngine.java
+++ src/main/java/com/saransh/snakesandladders/snakes_and_ladders/GameEngine.java
@@ -3,7 +3,9 @@ package com.saransh.snakesandladders.snakes_and_ladders;
 public class GameEngine {
 
     public int move(int currentPosition, int diceOutcome) {
-        return currentPosition + diceOutcome;
+    	int tentative = currentPosition + diceOutcome;
+        if (tentative > 100) return currentPosition;
+        return tentative;
     }
 }
 
